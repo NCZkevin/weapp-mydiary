5265811bf5b4b42e2e29cb5f2a89ef1a25f0a645
nczkevin <z1376871863@163.com>
2016-12-31 15:45:53 +0800

    editor 90%

--- pages/editor/editor.js
+++ pages/editor/editor.js
@@ -1,6 +1,35 @@
 // pages/editor/editor.js
 Page({
-  data:{},
+  data: {
+    array: ['美国', '中国', '巴西', '日本'],
+    objectArray: [
+      {
+        id: 0,
+        name: '美国'
+      },
+      {
+        id: 1,
+        name: '中国'
+      },
+      {
+        id: 2,
+        name: '巴西'
+      },
+      {
+        id: 3,
+        name: '日本'
+      }
+    ],
+    index: 0,
+    date: '2016-09-01',
+    time: '12:01'
+  },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
