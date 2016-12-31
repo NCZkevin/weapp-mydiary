47797206439c90d03e0582f796b94f673dace652
nczkevin <z1376871863@163.com>
2016-12-31 15:12:18 +0800

    index toollist

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,57 +1,71 @@
 //index.js
-var app = getApp()  
-Page( {  
-  data: {  
+var app = getApp()
+Page({
+  data: {
     /** 
         * 页面配置 
-        */  
-    winWidth: 0,  
-    winHeight: 0,  
+        */
+    winWidth: 0,
+    winHeight: 0,
     // tab切换  
-    currentTab: 0,  
-  },  
-  onLoad: function() {  
-    var that = this;  
-  
+    currentTab: 0,
+  },
+  onLoad: function () {
+    var that = this;
+
     /** 
      * 获取系统信息 
-     */  
-    wx.getSystemInfo( {  
-  
-      success: function( res ) {  
-        that.setData( {  
-          winWidth: res.windowWidth,  
-          winHeight: res.windowHeight  
-        });  
-      }  
-  
-    });  
-  },  
+     */
+    wx.getSystemInfo({
+
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+
+    });
+  },
   /** 
      * 滑动切换tab 
-     */  
-  bindChange: function( e ) {  
-  
-    var that = this;  
-    that.setData( { currentTab: e.detail.current });  
-  
-  },  
+     */
+  bindChange: function (e) {
+
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+
+  },
   /** 
    * 点击tab切换 
-   */  
-  swichNav: function( e ) {  
-  
-    var that = this;  
-  
-    if( this.data.currentTab === e.target.dataset.current ) {  
-      return false;  
-    } else {  
-      that.setData( {  
-        currentTab: e.target.dataset.current  
-      })  
-    }  
-  }  
-})  
+   */
+  swichNav: function (e) {
+
+    var that = this;
+
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
+  /** 
+   * 点击show diary
+   */
+  showDiary: function () {
+    wx.showModal({
+      title: '提示',
+      content: '这是一个模态弹窗',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        }
+      }
+    })
+  }
+})
 //获取应用实例
 // var app = getApp()
 // Page({
