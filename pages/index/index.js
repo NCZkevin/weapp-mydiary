bb6b170aa0db2350b94f65e09cea385324ba7eb0
nczkevin <z1376871863@163.com>
2016-12-31 14:25:09 +0800

    FE 80%

---
+++ pages/index/index.js
@@ -0,0 +1,79 @@
+//index.js
+var app = getApp()  
+Page( {  
+  data: {  
+    /** 
+        * 页面配置 
+        */  
+    winWidth: 0,  
+    winHeight: 0,  
+    // tab切换  
+    currentTab: 0,  
+  },  
+  onLoad: function() {  
+    var that = this;  
+  
+    /** 
+     * 获取系统信息 
+     */  
+    wx.getSystemInfo( {  
+  
+      success: function( res ) {  
+        that.setData( {  
+          winWidth: res.windowWidth,  
+          winHeight: res.windowHeight  
+        });  
+      }  
+  
+    });  
+  },  
+  /** 
+     * 滑动切换tab 
+     */  
+  bindChange: function( e ) {  
+  
+    var that = this;  
+    that.setData( { currentTab: e.detail.current });  
+  
+  },  
+  /** 
+   * 点击tab切换 
+   */  
+  swichNav: function( e ) {  
+  
+    var that = this;  
+  
+    if( this.data.currentTab === e.target.dataset.current ) {  
+      return false;  
+    } else {  
+      that.setData( {  
+        currentTab: e.target.dataset.current  
+      })  
+    }  
+  }  
+})  
+//获取应用实例
+// var app = getApp()
+// Page({
+//   data: {
+//     motto: 'Hello World',
+//     userInfo: {}
+//   },
+//   //事件处理函数
+//   bindViewTap: function() {
+//     wx.navigateTo({
+//       url: '../logs/logs'
+//     })
+//   },
+//   onLoad: function () {
+//     console.log('onLoad')
+//     var that = this
+//     //调用应用实例的方法获取全局数据
+//     app.getUserInfo(function(userInfo){
+//       //更新数据
+//       that.setData({
+//         userInfo:userInfo
+//       })
+//     })
+//   }
+// })
